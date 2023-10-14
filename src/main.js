963910e7f9cacbde1a10a5d9209ae0c55ff5ce5b
IntermediaDesigns <intermediadesignsllc@gmail.com>
2023-10-14 14:50:59 -0400

    2

--- src/main.js
+++ src/main.js
@@ -1,9 +1,4 @@
-import '../css/modern-normalize.css';
 import '../css/style.css';
 import '../css/components/header.css';
-import '../css/components/mobile-nav.css';
 import '../css/utils.css';
 
-import mobileNav from './utils/mobile-nav';
-
-mobileNav();
